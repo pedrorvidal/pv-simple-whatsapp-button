bc20e78347c019532cc19a014ea3023163086e7d
Pedro Vidal <pedrorvidal@gmail.com>
2026-09-02 15:15:34 -0300

    initial commit

---
+++ pv-simple-whatsapp-button.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: PV Simple WhatsApp Button
+ * Description: Botão flutuante de WhatsApp configurável (número e mensagem).
+ * Version: 1.0.0
+ * Author: Seu Nome
+ * License: GPL v2 or later
+ * Text Domain: pv-simple-whatsapp-button
+ */
+
+// Impede acesso direto ao arquivo (segurança básica)
+if (! defined('ABSPATH')) {
+    exit;
+}
